ce8b43ac09dd7cfbf3498f798dff7d61ba292ca8
Benny Pritchard <benjamintp.1985@gmail.com>
2020-12-24 15:26:55 +1100

    ScreenSaver App

---
+++ ScreenSaver/ScreenSaver/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace ScreenSaver
+{
+	partial class FormScreenSaver
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.Timer = new System.Windows.Forms.Timer(this.components);
+			this.SuspendLayout();
+			// 
+			// Timer
+			// 
+			this.Timer.Enabled = true;
+			this.Timer.Interval = 6000;
+			this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+			// 
+			// FormScreenSaver
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "FormScreenSaver";
+			this.ShowInTaskbar = false;
+			this.Text = "ScreenSaver";
+			this.TopMost = true;
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.FormScreenSaver_Load);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormScreenSaver_Paint);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormScreenSaver_KeyDown);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Timer Timer;
+	}
+}
+
---
+++ ScreenSaver/ScreenSaver/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+	public partial class FormScreenSaver : Form
+	{
+		List<Image> BGImages = new List<Image>();
+		List<Pics> BritPics = new List<Pics>();
+		Random rand = new Random();
+		
+		class Pics
+		{
+			public int PicNum;
+			public float x;
+			public float y;
+			public float Speed;
+
+		}
+		public FormScreenSaver()
+		{
+			InitializeComponent();
+		}
+
+		private void FormScreenSaver_Load(object sender, EventArgs e)
+		{
+			String[] images = System.IO.Directory.GetFiles("Pics");
+			foreach(string image in images)
+			{
+				BGImages.Add(new Bitmap(image));
+			}
+
+			for (int i = 0; i < 1000; i++)
+			{
+				Pics mp = new Pics();
+				mp.PicNum = i % BGImages.Count;
+				mp.x = rand.Next(0, Width);
+				mp.y = rand.Next(0, Height);
+
+				BritPics.Add(mp);
+			}
+		}
+
+		private void FormScreenSaver_KeyDown(object sender, KeyEventArgs e)
+		{
+			Close();
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			Refresh();
+
+		}
+
+		private void FormScreenSaver_Paint(object sender, PaintEventArgs e)
+		{
+			foreach (Pics bp in BritPics)
+			{
+				e.Graphics.DrawImage(BGImages[bp.PicNum], bp.x, bp.y);
+				bp.x -= 2;
+
+				if(bp.x < -250)
+				{
+					bp.x = Width + rand.Next(20, 100);
+				}
+			}
+		}
+	}
+}
